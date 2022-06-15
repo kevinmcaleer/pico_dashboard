e6c396a9a1349f682d5dd46ad521947e26604022
Kevin McAleer <kevinmcaleer@gmail.com>
2022-06-15 22:44:20 +0100

    live data feed working

--- chart.py
+++ chart.py
@@ -1,5 +1,3 @@
-import picodisplay2 as display
-
 class Chart:
     __title = ''
     __x_label = ''
@@ -9,14 +7,13 @@ class Chart:
     __y_scale = 1
     __x_offset = 0
     __y_offset = 0
-    __x_range = 0
-    __y_range = 0
     __background_colour = {'red': 0, 'green': 0, 'blue': 0}
     __grid_colour = {'red': 0, 'green': 0, 'blue': 0}
     __border_colour = {'red': 0, 'green': 0, 'blue': 0}
     __title_colour = {'red': 0, 'green': 0, 'blue': 0}
     __data_colour = {'red': 0, 'green': 0, 'blue': 0}
-    __data_point_radius = 1
+    __data_point_radius = 2
+    __data_point_radius2 = __data_point_radius * 4
     __data_point_width = 10
     __data_point_height = 1
     __data_point_x_offset = 0
@@ -25,24 +22,65 @@ class Chart:
     __y = 0
     __width = 100
     __height = 100
-    __border_width = 2
-    __text_height = 40
+    border_width = 2
+    __text_height = 16
+    __show_datapoints = False
+    __show_lines = False
+    __show_bars = True
+    grid = True
+    grid_spacing = 10
 
-    def __init__(self, title, x_label, y_label, x_values):
-        self.__title = title
-        self.__x_label = x_label
-        self.__y_label = y_label
-        self.__x_values = x_values
+    def __init__(self, display, title=None, x_label=None, y_label=None, x_values=None, y_values=None):
+        self.display = display
+        if title: self.__title = title
+        if x_label: self.__x_label = x_label
+        if y_label: self.__y_label = y_label
+        if x_values:
+            # Scale the data
+            self.__x_values = x_values
+            self.min_val = min(self.__x_values)  # get the minimum value
+            self.max_val = max(self.__x_values)  # get the maximum value
+        if y_values: self.__y_values = y_values
 
     @property
-    def border_width(self):
-        """ Get the border width """
-        return self.__border_width
+    def show_bars(self):
+        """ Get the show_bars value """
+        return self.__show_bars
 
-    @border_width.setter
-    def border_width(self, border_width):
-        """ Set the border width """
-        self.__border_width = border_width
+    @show_bars.setter
+    def show_bars(self, value):
+        """ Set the show_bars value """
+        self.__show_bars = value
+
+    @property
+    def show_datapoints(self):
+        """ Get Show the data points status """
+        return self.__show_datapoints
+
+    @show_datapoints.setter
+    def show_datapoints(self, value):
+        """ Set Show the data points status"""
+        self.__show_datapoints = value
+
+    @property
+    def show_lines(self):
+        """ Get Show the lines status """
+        return self.__show_lines
+
+    @show_lines.setter
+    def show_lines(self, value):
+        """ Set Show the lines status"""
+        self.__show_lines = value
+
+    @property
+    def data_point_radius(self):
+        """ Get the radius of the data points """
+        return self.__data_point_radius
+
+    @data_point_radius.setter
+    def data_point_radius(self, value):
+        """ Set the radius of the data points """
+        self.__data_point_radius = value
 
     @property
     def x(self):
@@ -114,21 +152,21 @@ class Chart:
         """ Set the border colour """
         self.__border_colour = colour
         
-    @property
-    def x_scale(self):
-        return self.__x_scale
-
-    @x_scale.setter
-    def x_scale(self, value):
-        self.__x_scale = value
-
-    @property
-    def y_scale(self):
-        return self.__y_scale
-
-    @y_scale.setter
-    def y_scale(self, value): 
-        self.__y_scale = value
+#     @property
+#     def x_scale(self):
+#         return self.__x_scale
+# 
+#     @x_scale.setter
+#     def x_scale(self, value):
+#         self.__x_scale = value
+# 
+#     @property
+#     def y_scale(self):
+#         return self.__y_scale
+# 
+#     @y_scale.setter
+#     def y_scale(self, value): 
+#         self.__y_scale = value
 
     @property
     def x_offset(self):
@@ -138,16 +176,25 @@ class Chart:
     def x_offset(self, value):
         self.__x_offset = value
 
-    def set_y_offset(self, value):
+    @property
+    def y_offset(self, value):
         self.__y_offset = value
+    
+    @y_offset.setter
+    def y_offset(self, value):
+        self.__y_offset = value
+    
+    @property
+    def x_values(self):
+        return self.__x_values
 
-    def set_x_range(self, value):
-        self.__x_range = value
-
-    def set_y_range(self, value):
-        self.__y_range = value
+    @x_values.setter
+    def x_values(self, value):
+        """ Set the x values """
+        self.__x_values = value
 
     def set_x_axis_colour(self, red, green, blue):
+        """ Set the x axis colour """
         self.x_axis_colour = {'red': red, 'green': green, 'blue': blue}
 
     @property
@@ -159,57 +206,143 @@ class Chart:
     def title(self, title):
         """ Set the title """
         self.__title = title
+
+    @property
+    def data_point_radius2(self):
+        """ Get the radius of the data points """
+        return self.__data_point_radius2
+
+    @data_point_radius2.setter
+    def data_point_radius2(self, value):
+        """ Set the radius of the data points """
+        self.__data_point_radius2 = value
         
     def draw_border(self):
-        display.set_pen(self.__border_colour['red'], self.__border_colour['green'], self.__border_colour['blue'])
+        self.display.set_pen(self.__border_colour['red'], self.__border_colour['green'], self.__border_colour['blue'])
         x = self.__x
         y = self.__y 
-        w = self.__width
-        h = self.__height
-        display.rectangle(x, y, w, h)
-        display.set_pen(self.__background_colour['red'], self.__background_colour['green'], self.__background_colour['blue'])
+        w = self.__width #- (self.border_width * 2) 
+        h = self.__height #- (self.border_width * 2)
+        x1 = x+w
+        y1 = y+h
+        # self.display.rectangle(x, y, w, h)
+        
+#         print(f'x: {x}, y: {y}, w: {w}, h: {h}, x1: {x1}, y1: {y1}')
 
-        x = self.__x + self.border_width
-        y = self.__y + self.border_width
-        w = self.__width - (self.__border_width * 2)
-        h = self.__height - (self.__border_width * 2)
-        display.rectangle(x, y, w, h)
+        for i in range(0,self.border_width,1):
+#             print(f'border width: {i}')
+            self.display.line(x+i, y+i, x+i, y1-i) # left
+            self.display.line(x+i, y+i, x1-i, y+i) # top
+            self.display.line(x+i, y1-i-1, x1-i, y1-i-1) # bottom
+            self.display.line(x1-i-1, y+i, x1-i-1, y1) # right
+        self.display.set_pen(self.__background_colour['red'], self.__background_colour['green'], self.__background_colour['blue'])
         
         # Draw the border
-        display.update()
+        self.display.update()
+
+    def draw_grid(self):
+        self.display.set_pen(self.__border_colour['red']//4, self.__border_colour['green']//4, self.__border_colour['blue']//4)
+        x = self.x
+        y = self.y 
+        w = self.width
+        h = self.height
+
+        cols = w // self.grid_spacing
+        row = h // self.grid_spacing
+
+#         print(f'cols: {cols}, row: {row}')
+
+        for i in range(cols):
+            self.display.line(x+self.grid_spacing*i, y, x+self.grid_spacing*i, y+h)
+            # print(f'drawing cols: {i}')    
+        
+        for j in range(row):
+            self.display.line(x, y+self.grid_spacing*j, x+w, y+self.grid_spacing*j)
+            # print(f'drawing row: {j}')  
+        
+        # Update display
+        self.display.update()
+
+    def map(self, x, in_min, in_max, out_min, out_max):
+        """ Map a value from one range to another """
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+    def scale_data(self):
+
+        self.min_val = min(self.__x_values)  # get the minimum value
+        self.max_val = max(self.__x_values)  # get the maximum value
+        
+        # Calculate the scale
+        if self.max_val - self.min_val == 0:
+            self.max_val = 2
+            self.min_val = 1
+        self.__y_scale = (((self.__height - self.__text_height) - self.border_width * 2)) // (self.max_val - self.min_val)
+#         print(f'Min: {self.min_val} Max: {self.max_val}, Y Scale: {self.__y_scale}')
 
     def update(self):
-        # Draw border
-        self.draw_border()
-
-        display.set_pen(self.__title_colour['red'], self.__title_colour['green'], self.__title_colour['blue'])
-        display.text(self.title, self.x+self.__border_width+1, self.y + self.__border_width+1,self.__width)
-
-        # Update the display
-        # display.update()
-
-        # Scale the data
-        min_val = min(self.__x_values)  # get the minimum value
-        max_val = max(self.__x_values)  # get the maximum value
-
-        # Calculate the scale
-        self.__y_scale = (((self.__height - self.__text_height) - self.__border_width * 2)) / (max_val - min_val)
+        """ Update the chart """
         
-        # print(f'Min: {min_val} Max: {max_val}, Scale: {self.__y_scale}')
+        self.display.set_clip(self.x, self.y, self.width, self.height)
+        self.display.clear()
+        self.display.remove_clip()
+        
+        # Draw the Grid
+        if self.grid: self.draw_grid()
 
-        display.set_pen(self.__data_colour['red'], self.__data_colour['green'], self.__data_colour['blue'])
+#         self.scale_data()
+        
+        gap = 3
+        self.display.set_clip(self.x+gap, self.y+gap, self.width-gap, self.height-gap)
 
-        self.__x_offset = self.__x+self.__border_width+1
-        self.__y_offset = (self.__height-self.__border_width)-1
+        self.display.set_pen(self.__title_colour['red'], self.__title_colour['green'], self.__title_colour['blue'])
+#         self.display.rectangle(self.x, self.y, self.x+self.width, self.y+self.__text_height)
+#         
+#         self.display.set_pen(0,0,0)
+        self.display.text(self.title, self.x+self.border_width+1, self.y + self.border_width+1,self.__width)
+
+        self.display.set_pen(self.__data_colour['red'], self.__data_colour['green'], self.__data_colour['blue'])
+
+        self.__x_offset = self.__x+self.border_width+2
+        self.__y_offset = (self.__height-self.border_width)-2
 
         x_pos = self.x + self.__x_offset
         y_pos = self.y + self.__y_offset 
-        for item in self.__x_values:
-            item = int(item * self.__y_scale) # scale the data
-            data_height = int(item)
-            display.rectangle(x_pos, y_pos-item, self.__data_point_width, data_height)
-            print(item)
-            x_pos += self.__data_point_width + 1
-            print(f'x_pos: {x_pos}, y_pos: {y_pos}')
 
-        display.update()
+        prev_x = x_pos
+        prev_y = y_pos
+        
+        self.display.remove_clip()
+
+        # The area within the chart
+        plot_area = (self.height - self.__text_height) - self.border_width*2
+        self.display.set_clip(self.x+self.border_width, self.y+self.border_width+self.__text_height, self.y+self.width, (self.y+self.height)-self.border_width)
+#         self.display.line(self.x, self.y+self.height - plot_area, self.width, self.y+self.height-plot_area)
+        # print(f'plot area: {plot_area}, height: {self.height}')
+        for item in self.__x_values:
+
+            item = int(self.map(item, self.min_val, self.max_val,0,plot_area)) # scale the data
+#             print(f'item: {item}')
+            data_height = int(item)
+
+            if self.show_bars:
+                self.display.rectangle(x_pos, y_pos-item, self.__data_point_width, data_height)
+            if self.show_datapoints:
+                self.display.set_pen(self.__data_colour['red']//4, self.__data_colour['green']//4, self.__data_colour['blue']//4)
+                self.display.circle(x_pos, y_pos-item, self.__data_point_radius2)
+                self.display.set_pen(self.__data_colour['red'], self.__data_colour['green'], self.__data_colour['blue'])
+                self.display.circle(x_pos, y_pos-item, self.__data_point_radius)
+                
+            if self.show_lines:
+                self.display.line(x_pos, y_pos-item, prev_x, prev_y)
+
+            prev_x = x_pos
+            prev_y = y_pos-item
+            x_pos += self.__data_point_width+1
+
+        self.display.remove_clip()
+        
+        # Draw the border
+        self.draw_border()
+
+        self.display.update()
+        
--- dashboard.py
+++ dashboard.py
@@ -2,24 +2,27 @@
 # Kevin McAleer
 # June 2022
 
-import picodisplay2 as display
+# import picodisplay2 as display
 from chart import Chart
+from st7789 import ST7789
 import gc 
-width = display.get_width()
-height = display.get_height()
-print(f"width: {width}, height {height}")
+import machine
+from time import sleep
+WIDTH, HEIGHT = 320, 240 # Pico Display 2.0
+
+display = ST7789(WIDTH, HEIGHT, rotate180=False)
+
 gc.collect()
-display_buffer = bytearray(width * height * 2)
-display.init(display_buffer)
 
 # Set the display backlight to 50%
-display.set_backlight(0.5)
+display.set_backlight(1.0)
 
 data = [10,11,9,13,2,10,12,10,10,9,7,11,10,11,9,13,2,10,12,10,10,9,7,11]
 
 # Set the theme colour
 THEME = {'red': 255, 'green': 126, 'blue': 121}
 THEME2 = {'red': 252, 'green': 210, 'blue': 0}
+THEME3 = {'red': 151, 'green': 250, 'blue': 121}
 
 # Frame background colour
 BACKGROUND = {'red': 100, 'green': 100, 'blue': 100}
@@ -27,59 +30,90 @@ BACKGROUND = {'red': 100, 'green': 100, 'blue': 100}
 def draw_data():
     """ render the data on the graph, scaling to the axix """
 
-def frame(x, y, w, h, colour):
-    """ Draw a frame on the display """
-
-    print(f'x: {x}, y: {y}, w: {w}, h: {h}')
-    # Draw a frame
-    display.set_pen(colour['red'], colour['green'], colour['blue'])
-    display.rectangle(x, y, w, h)
-#     display.clear()
-
-    frame_width = 1
-
-    display.set_pen(0, 0, 0)
-    x1 = x+frame_width
-    y1 = y+frame_width
-    w1 = w-frame_width*2
-    h1 = h-frame_width*2
-    
-    display.rectangle(x1, y1, w1, h1)
-    print(f'x1: {x1}, y1: {y1}, w1: {w1}, h1: {h1}')
-
-    display.set_pen(255, 255, 255)
-    display.text("Pico Dashboard", 2, 2, w)
-
-    # time to update the display
-    display.update()
-
-    bar_width = 5
-
 border = 20
 # frame(border,border,width-(border*2),height-(border*2),BACKGROUND)
 
-temp_chart = Chart('Temperature', 'Time', 'Temperature', data)
-temp_chart2 = Chart('Humidity', 'Time', 'Temperature', data)
+chart1 = Chart(display, title='Temperature', x_values=data)
+chart2 = Chart(display, title='Humidity', x_values=data)
+chart3 = Chart(display, title="Pressure", x_values=data)
 
-temp_chart.data_colour = THEME
-temp_chart2.data_colour = THEME2
-temp_chart.title_colour = THEME
-temp_chart2.title_colour = THEME2
-temp_chart2.data_colour = THEME2
-temp_chart.border_colour = BACKGROUND
-temp_chart2.border_colour = BACKGROUND
-temp_chart.border_width = 1
-temp_chart2.border_width = 1
+chart1.data_colour = THEME
+chart2.data_colour = THEME2
+chart3.data_colour = THEME3
 
-temp_chart.x = 0
-temp_chart.y = 0
-temp_chart.width = display.get_width()
-temp_chart.height = display.get_height()//2
+chart1.title_colour = THEME
+chart2.title_colour = THEME2
+chart3.title_colour = THEME3
 
-temp_chart2.x = 0
-temp_chart2.y = (display.get_height()//2) + 1
-temp_chart2.width = display.get_width()
-temp_chart2.height = display.get_height()//2 - 1
+chart1.border_colour = chart2.border_colour = chart3.border_colour = BACKGROUND
+# chart2.border_colour = BACKGROUND
+#  = BACKGROUND
 
-temp_chart.update()
-temp_chart2.update()
+chart1.border_width = 1
+chart2.border_width = 1
+chart3.border_width = 1
+
+chart1.x = 0
+chart1.y = 0
+chart1.width = WIDTH
+chart1.height = HEIGHT//3
+
+chart2.x = 0
+chart2.y = (HEIGHT//3) + 1
+chart2.width = WIDTH
+chart2.height = HEIGHT//3 - 1
+chart2.data_point_radius = 2
+
+chart3.x = 0
+chart3.y = chart2.y + chart2.height + 1
+chart3.width = WIDTH 
+chart3.height = HEIGHT//3 -1
+
+chart2.show_bars = False
+chart2.show_lines = True
+chart3.data_point_radius = 2
+
+chart3.show_datapoints = True
+chart3.show_bars = False
+# chart3.show_lines = True
+
+chart1.grid = True
+
+chart1.update()
+chart2.update()
+chart3.update()
+
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
+
+data = [28,34]
+chart1.x_values = data
+chart2.x_values = data
+chart3.x_values = data
+chart1.scale_data()
+chart2.scale_data()
+chart3.scale_data()
+
+data = []
+
+chart1.x_values = data
+chart2.x_values = data
+chart3.x_values = data
+
+
+while True or KeyboardInterrupt:
+    reading = sensor_temp.read_u16() * conversion_factor 
+    temperature = int(27 - (reading - 0.706)/0.001721)
+    data.append(temperature)
+    print(data)
+    if len(data) == 29:
+        data.pop(0)
+    chart1.x_values = data
+    chart2.x_values = data
+    chart3.x_values = data
+    print(chart1.x_values)#
+    chart1.update()
+    chart2.update()
+    chart3.update()
+    sleep(0.5)
+
